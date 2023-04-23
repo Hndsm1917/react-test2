ed7392beb4ebca2d146b0f84d50d51d446043674
Np <np@develup.pro>
2023-04-23 17:21:17 +0300

    Feature: initial commit

---
+++ vue/src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="layout">
+    <LayoutResiser class="layout__resiser"></LayoutResiser>
+  </div>
+
+</template>
+
+<script setup>
+import LayoutResiser from './components/LayoutResiser.vue'
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ vue/src/main.js
@@ -0,0 +1,7 @@
+import {
+  createApp
+} from 'vue'
+import './assets/styles/app.scss';
+import App from './App.vue'
+
+createApp(App).mount('#app')
